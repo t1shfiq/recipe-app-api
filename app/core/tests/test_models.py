86a1f80d202b170acfc32e948a4abf1b8785150c
Ali Tashfiq Wazed <ali.tashfiq4@gmail.com>
2022-06-14 18:36:26 -0400

    Added superuser support

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -19,3 +19,31 @@ class ModelTests(TestCase):
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """Test the email for a new user is normalized."""
+        samples_emails = [
+            ['test1@EXAMPLE.com', 'test1@example.com'],
+            ['Test2@Example.com', 'Test2@example.com'],
+            ['TEST3@EXAMPLE.COM', 'TEST3@example.com'],
+            ['test4@example.COM', 'test4@example.com'],
+        ]
+
+        for email, normalized_email in samples_emails:
+            user = get_user_model().objects.create_user(email,'test123',)
+            self.assertEqual(user.email, normalized_email)
+
+    def test_create_user_with_email_invalid(self):
+        """Test creating a user with an invalid email raises error."""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, 'test123')
+
+    def test_create_superuser(self):
+        """Test creating a superuser."""
+        user = get_user_model().objects.create_superuser(
+            'test@example.com',
+            'test123'
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
